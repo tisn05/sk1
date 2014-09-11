ea53e67f036c57aacf57cacaafd073616b75481b
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-09-11 15:43:02 +0000

    STOCK_PDXF_DOC icon refactored as STOCK_SK1_DOC

--- trunk/src/sk1/widgets/docarea.py
+++ trunk/src/sk1/widgets/docarea.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2011-2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 
@@ -109,7 +109,7 @@ class TabCaption(gtk.HBox):
 
 		self.label = gtk.Label('')
 		self.tab_icon = gtk.Image()
-		self.tab_icon.set_from_stock(icons.STOCK_PDXF_DOC, gtk.ICON_SIZE_MENU)
+		self.tab_icon.set_from_stock(icons.STOCK_SK1_DOC, gtk.ICON_SIZE_MENU)
 		self.but_icon = gtk.Image()
 		self.but_icon.set_from_stock(gtk.STOCK_CLOSE, gtk.ICON_SIZE_MENU)
 
