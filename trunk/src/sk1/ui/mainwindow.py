a2171df80876c633838553a82fc2168b51ee55b1
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-09-11 15:08:52 +0000

    ui modules refactored in ui package

--- trunk/src/sk1/ui/mainwindow.py
+++ trunk/src/sk1/ui/mainwindow.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2011-2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import gtk
 
 from sk1 import config, events
-from sk1.menubar import AppMenubar
-from sk1.toolbar import AppToolbar
-from sk1.tools import AppTools
-from sk1.palette import Palette
-from sk1.statusbar import AppStatusbar
+from sk1.ui.menubar import AppMenubar
+from sk1.ui.toolbar import AppToolbar
+from sk1.ui.tools import AppTools
+from sk1.ui.palette import Palette
+from sk1.ui.statusbar import AppStatusbar
 from sk1.context import ContextPanel
 from sk1.plugins import PluginPanel
 from sk1.widgets.hidable import HidableVBox
