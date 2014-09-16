69f865224aacda3e3dc2ec40f83e29b0543c9287
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-16 08:00:48 +0000

    PanelHolder removed

--- src/sk1/context/__init__.py
+++ src/sk1/context/__init__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2013 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, gtk
 
@@ -54,8 +54,7 @@ class ContextPanel(gtk.HBox):
 		self.insp = self.app.inspector
 
 		self.set_border_width(2)
-		self.holder = PanelHolder()
-		self.pack_start(self.holder, False, False, 0)
+		self.set_size_request(-1, 33)
 
 		for item in PLUGINS:
 			plg = item(self.mw)
@@ -100,12 +99,6 @@ class ContextPanel(gtk.HBox):
 			else:
 				return DEFAULT
 
-
-class PanelHolder(gtk.Image):
-	def __init__(self):
-		gtk.Image.__init__(self)
-		image_file = os.path.join(config.resource_dir, 'icons', 'panel-holder.png')
-		self.set_from_pixbuf(gtk.gdk.pixbuf_new_from_file(image_file))
 
 
 
