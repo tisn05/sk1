fc956af3e8b77c5d5a658ee31efda18cef43437f
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-26 17:59:34 +0000

    image ids

---
+++ src/wal/image_ids.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2014 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+IMG_APP_ICON = 'sk1-app-icon'
+IMG_CAIRO_BANNER = 'sk1-cairo-banner'
+IMG_SPLASH_TRIADA = 'sk1-splash-triada'
+
+IMG_RULER_BG = 'sk1-ruler-corner-bg'
+IMG_RULER_DO_LL = 'sk1-ruler-docorigin-ll'
+IMG_RULER_DO_LU = 'sk1-ruler-docorigin-lu'
+IMG_RULER_DO_C = 'sk1-ruler-docorigin-c'
+
+IMG_PAGER_END = 'sk1-pager-end'
+IMG_PAGER_NEXT = 'sk1-pager-next'
+IMG_PAGER_PREV = 'sk1-pager-prev'
+IMG_PAGER_START = 'sk1-pager-start'
+
+IMG_PREFS_CMS = 'sk1-prefs-cms'
+IMG_PREFS_CMS_BANNER = 'sk1-prefs-cms-banner'
+IMG_PREFS_RULER = 'sk1-prefs-ruler'
+IMG_PREFS_PALETTE = 'sk1-prefs-palette'
+
+IMG_CTX_JUMP = 'sk1-ctx-jump'
+IMG_CTX_UNITS = 'sk1-ctx-units'
+IMG_CTX_LANDSCAPE = 'sk1-ctx-page-landscape'
+IMG_CTX_PORTRAIT = 'sk1-ctx-page-portrait'
+IMG_CTX_ROTATE = 'sk1-ctx-rotate-selection'
+IMG_KEEP_RATIO = 'sk1-ctx-keep-ratio'
+IMG_DONT_KEEP_RATIO = 'sk1-ctx-dont-keep-ratio'
+
+IMG_PALETTE_ARROW_BOTTOM = 'sk1-palette-arrow-bottom'
+IMG_PALETTE_ARROW_LEFT = 'sk1-palette-arrow-left'
+IMG_PALETTE_ARROW_RIGHT = 'sk1-palette-arrow-right'
+IMG_PALETTE_ARROW_TOP = 'sk1-palette-arrow-top'
+IMG_PALETTE_CMYK_COLOR = 'sk1-palette-cmyk_color'
+IMG_PALETTE_DOUBLE_ARROW_BOTTOM = 'sk1-palette-double-arrow-bottom'
+IMG_PALETTE_DOUBLE_ARROW_LEFT = 'sk1-palette-double-arrow-left'
+IMG_PALETTE_DOUBLE_ARROW_RIGHT = 'sk1-palette-double-arrow-right'
+IMG_PALETTE_DOUBLE_ARROW_TOP = 'sk1-palette-double-arrow-top'
+IMG_PALETTE_NO_COLOR = 'sk1-palette-no-color'
+IMG_PALETTE_RGB_COLOR = 'sk1-palette-rgb-color'
+
