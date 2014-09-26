5c3f968693da536cc367462ffbb5e9ed6767dd61
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-09-26 18:28:56 +0000

    rc methods

--- trunk/src/wal/rc.py
+++ trunk/src/wal/rc.py
@@ -15,6 +15,45 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import os, gtk
 
 PATH = os.path.dirname(os.path.abspath(__file__))
+ICONS_PATH = os.path.join(PATH, 'icons')
+GENERIC_ICONS_PATH = os.path.join(ICONS_PATH, 'generic')
+ICONS16_PATH = os.path.join(ICONS_PATH, '16x16')
+ICONS24_PATH = os.path.join(ICONS_PATH, '24x24')
+ICONS32_PATH = os.path.join(ICONS_PATH, '32x32')
+
+FIXED16 = gtk.icon_size_register('FIXED16', 16, 16)
+FIXED22 = gtk.icon_size_register('FIXED22', 22, 22)
+FIXED24 = gtk.icon_size_register('FIXED24', 24, 24)
+FIXED32 = gtk.icon_size_register('FIXED32', 32, 32)
+
+def get_image_path(image_id):
+	imgname = image_id + '.png'
+	if image_id[:4] == 'sk1-':
+		return os.path.join(GENERIC_ICONS_PATH, imgname)
+	return None
+
+def get_stock_pixbuf(image_id, size=FIXED16):
+	return gtk.Image().render_icon(image_id, size)
+
+def get_pixbuf(image_id, size=FIXED16):
+	if image_id[:4] == 'sk1-':
+		loader = gtk.gdk.pixbuf_new_from_file
+		return loader(get_image_path(image_id))
+	else:
+		return get_stock_pixbuf(image_id, size)
+
+def get_stock_image(image_id, size=FIXED16):
+	image = gtk.Image()
+	image.set_from_pixbuf(get_stock_pixbuf(image_id, size))
+
+def get_image(image_id, size=FIXED16):
+	if image_id[:4] == 'sk1-':
+		image = gtk.Image()
+		image.set_from_pixbuf(get_pixbuf(image_id))
+		return image
+	else:
+		return get_stock_image(image_id, size)
+	return image
