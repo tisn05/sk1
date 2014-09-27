456495ded5f836035f43a75a78727db12b5e2180
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-27 19:32:26 +0000

    toggle buttons and it's tests

--- wal-tests/test03_button.py
+++ wal-tests/test03_button.py
@@ -5,12 +5,14 @@ class MW(wal.MainWindow):
 	def __init__(self):
 		wal.MainWindow.__init__(self)
 		self.set_size(300, 200)
+
 		but = wal.Button(self, 'Test button', cmd=self.test)
-		but.connect('button-press-event', self.pressed)
+		self.pack(but, padding=10)
+
+		but = wal.FButton(self, 'Test button', cmd=self.test)
 		self.pack(but, padding=10)
 
 	def test(self, *args):print 'CLICKED!'
-	def pressed(self, *args):print 'PRESSED'
 
 
 mw = MW()
