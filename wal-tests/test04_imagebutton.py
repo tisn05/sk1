44d4a6ecf0224e0a0c32d9c8c160d1f850d5dffa
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-27 18:01:43 +0000

    tests improved

--- wal-tests/test04_imagebutton.py
+++ wal-tests/test04_imagebutton.py
@@ -4,10 +4,15 @@ class MW(wal.MainWindow):
 
 	def __init__(self):
 		wal.MainWindow.__init__(self)
-		but = wal.ImageButton(self, wal.IMG_PALETTE_ARROW_BOTTOM, 'test')
-		self.pack(but, padding=10)
-		but = wal.FlatImageButton(self, wal.IMG_PALETTE_DOUBLE_ARROW_BOTTOM, 'test')
-		self.pack(but, padding=10)
+		self.set_size(300, 200)
+		hbox = wal.HBox(self)
+		but = wal.ImgButton(hbox, wal.IMG_PALETTE_ARROW_BOTTOM,
+						'test', cmd=self.test)
+		hbox.pack(but, padding=10)
+		but = wal.FImgButton(hbox, wal.IMG_PALETTE_DOUBLE_ARROW_BOTTOM,
+						'test', cmd=self.test)
+		hbox.pack(but, padding=10)
+		self.pack(hbox)
 
 	def test(self, *args):print 'CLICKED!'
 
--- wal-tests/test05_labels.py
+++ wal-tests/test05_labels.py
@@ -4,6 +4,7 @@ class MW(wal.MainWindow):
 
 	def __init__(self):
 		wal.MainWindow.__init__(self)
+		self.set_size(300, 200)
 		self.pack(wal.Label(self, 'Test Label'), padding=10)
 		self.pack(wal.DecorLabel(self, 'Test DecorLabel', italic=True), padding=10)
 		self.pack(wal.DecorLabel(self, 'Test DecorLabel', italic=True, bold=True), padding=10)
