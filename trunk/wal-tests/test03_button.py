53bfdf6e8acebbfa51ba9eb42b48addfb5f46b3b
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-09-27 18:01:43 +0000

    tests improved

--- trunk/wal-tests/test03_button.py
+++ trunk/wal-tests/test03_button.py
@@ -4,6 +4,7 @@ class MW(wal.MainWindow):
 
 	def __init__(self):
 		wal.MainWindow.__init__(self)
+		self.set_size(300, 200)
 		but = wal.Button(self, 'Test button', cmd=self.test)
 		but.connect('button-press-event', self.pressed)
 		self.pack(but, padding=10)
