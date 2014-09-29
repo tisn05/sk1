298d5eeefecdc16d9d24f48bfc774386395b0cca
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-29 04:51:45 +0000

    HidableHBox and HidableVBox test

---
+++ wal-tests/test09_hidable_boxes.py
@@ -0,0 +1,35 @@
+import wal
+
+class MW(wal.MainWindow):
+
+	def __init__(self):
+		wal.MainWindow.__init__(self)
+		self.set_size(300, 200)
+
+		self.hor_box = wal.HidableHBox(self)
+		self.pack(self.hor_box)
+
+		self.hor_box.pack(wal.Button(self.hor_box, 'HBox'), True, True)
+		self.hor_box.set_visible(True)
+
+		hbox = wal.HBox(self)
+		self.pack(hbox, True, True)
+
+		self.vert_box = wal.HidableVBox(hbox)
+		self.vert_box.pack(wal.Button(self.vert_box, 'VBox'), True, True)
+		self.vert_box.set_visible(True)
+		hbox.pack(self.vert_box)
+
+		vbox = wal.VBox(hbox)
+		hbox.pack(vbox, end=True)
+		vbox.pack(wal.Button(vbox, 'Test HBox', cmd=self.test_hbox))
+		vbox.pack(wal.Button(vbox, 'Test VBox', cmd=self.test_vbox))
+
+	def test_hbox(self, *args):
+		self.hor_box.set_visible(not self.hor_box.get_visible())
+
+	def test_vbox(self, *args):
+		self.vert_box.set_visible(not self.vert_box.get_visible())
+
+mw = MW()
+mw.run()
