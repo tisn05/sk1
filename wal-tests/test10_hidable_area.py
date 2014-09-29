191a911129a71dd8838d5d45844bead97b01a4bf
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-09-29 05:19:59 +0000

    HidableVArea test

---
+++ wal-tests/test10_hidable_area.py
@@ -0,0 +1,20 @@
+import wal
+
+class MW(wal.MainWindow):
+
+	def __init__(self):
+		wal.MainWindow.__init__(self)
+		self.set_size(300, 200)
+
+		self.area = wal.HidableVArea(self)
+		self.area.pack(wal.Button(self.area, 'HidableVArea'), True, True)
+		self.pack(self.area, True, True)
+		self.area.set_visible(True)
+
+		self.pack(wal.Button(self, 'Test HidableArea', cmd=self.test_area), end=True)
+
+	def test_area(self, *args):
+		self.area.set_visible(not self.area.get_visible())
+
+mw = MW()
+mw.run()
