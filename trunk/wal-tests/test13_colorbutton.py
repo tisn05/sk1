cb13ed07f5696f8d2bf0add215cdcde3b966c1e0
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-09-29 22:55:56 +0000

    ColorButton test improvement

--- trunk/wal-tests/test13_colorbutton.py
+++ trunk/wal-tests/test13_colorbutton.py
@@ -7,13 +7,17 @@ class MW(wal.MainWindow):
 		self.set_size(300, 200)
 		hbox = wal.HBox(self)
 		self.pack(hbox)
-		hbox.pack(wal.ColorButton(hbox, (0, 0, 0), 'Select foreground',
-								cmd=self.callback), padding=30)
-		hbox.pack(wal.ColorButton(hbox, (1, 1, 1), 'Select background',
-								cmd=self.callback), padding=30)
+		self.cb1 = wal.ColorButton(hbox, (0, 0, 0), 'Select foreground',
+								cmd=self.callback)
+		hbox.pack(self.cb1, padding=30)
+		self.cb2 = wal.ColorButton(hbox, (1, 1, 1), 'Select background',
+								cmd=self.callback)
+		hbox.pack(self.cb2, padding=30)
 
 	def callback(self, *args):
 		print 'Changed!'
+		print self.cb1.get_color()
+		print self.cb2.get_color()
 
 mw = MW()
 mw.run()
