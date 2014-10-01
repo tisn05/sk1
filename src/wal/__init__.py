40164c36821b1ced6cba76619eaded2447623cac
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-10-01 07:08:10 +0000

    WAL external constants

--- src/wal/__init__.py
+++ src/wal/__init__.py
@@ -16,6 +16,7 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from const import *
 from image_ids import *
 
 from window import MainWindow, MW_Menu, MW_Toolbar
