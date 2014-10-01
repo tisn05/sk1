5bfd6b54c5e1311ef72979cdca31340614c3c115
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-10-01 09:52:25 +0000

    minor refactoring

--- trunk/wal-tests/test09_hidable_boxes.py
+++ trunk/wal-tests/test09_hidable_boxes.py
@@ -6,21 +6,21 @@ class MW(wal.MainWindow):
 		wal.MainWindow.__init__(self)
 		self.set_size(300, 200)
 
-		self.hor_box = wal.HidableHBox(self)
-		self.pack(self.hor_box)
+		self.hbox = wal.HidableHBox(self)
+		self.pack(self.hbox)
 
 		size = (50, 50)
 
-		self.hor_box.pack(wal.ColorPlate(self.hor_box, size, bg=wal.BLACK), True, True)
-		self.hor_box.set_visible(True)
+		self.hbox.pack(wal.ColorPlate(self.hbox, size, bg=wal.BLACK), True, True)
+		self.hbox.set_visible(True)
 
 		hbox = wal.HBox(self)
 		self.pack(hbox, True, True)
 
-		self.vert_box = wal.HidableVBox(hbox)
-		self.vert_box.pack(wal.ColorPlate(self.hor_box, size, bg=wal.GRAY), True, True)
-		self.vert_box.set_visible(True)
-		hbox.pack(self.vert_box)
+		self.vbox = wal.HidableVBox(hbox)
+		self.vbox.pack(wal.ColorPlate(self.hbox, size, bg=wal.GRAY), True, True)
+		self.vbox.set_visible(True)
+		hbox.pack(self.vbox)
 
 		vbox = wal.VBox(hbox)
 		hbox.pack(vbox, end=True)
@@ -28,10 +28,10 @@ class MW(wal.MainWindow):
 		vbox.pack(wal.Button(vbox, 'Test VBox', cmd=self.test_vbox))
 
 	def test_hbox(self, *args):
-		self.hor_box.set_visible(not self.hor_box.get_visible())
+		self.hbox.set_visible(not self.hbox.get_visible())
 
 	def test_vbox(self, *args):
-		self.vert_box.set_visible(not self.vert_box.get_visible())
+		self.vbox.set_visible(not self.vbox.get_visible())
 
 mw = MW()
 mw.run()
