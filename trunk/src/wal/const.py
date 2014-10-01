d0fbbc32ec77859af91442ecf81d1a6bf8b6558d
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-10-01 07:06:27 +0000

    wal external constants

---
+++ trunk/src/wal/const.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2014 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gconst
+
+#--- Mouse buttons
+LEFT_BUTTON = gconst.LEFT_BUTTON
+MIDDLE_BUTTON = gconst.MIDDLE_BUTTON
+RIGHT_BUTTON = gconst.RIGHT_BUTTON
+
+#--- Colors
+WHITE = (1.0, 1.0, 1.0)
+BLACK = (0.0, 0.0, 0.0)
+LIGHT_GRAY = (0.87, 0.87, 0.87)
+GRAY = (0.63, 0.63, 0.63)
+DARK_GRAY = (0.35, 0.35, 0.35)
+LIGHT_RED = (1.0, 0.75, 0.75)
+RED = (1.0, 0.0, 0.0)
+DARK_RED = (0.75, 0.0, 0.0)
+LIGHT_GREEN = (0.75, 1.0, 0.75)
+GREEN = (0.0, 1.0, 0.0)
+DARK_GREEN = (0.0, 0.5, 0.0)
+LIGHT_BLUE = (0.75, 0.75, 1.0)
+BLUE = (0.0, 0.0, 1.0)
+DARK_BLUE = (0.0, 0.0, 0.75)
+LIGHT_YELLOW = (1.0, 1.0, 0.75)
+YELLOW = (1.0, 1.0, 0.0)
+DARK_YELLOW = (0.75, 0.75, 0.0)
+LIGHT_CYAN = (0.75, 1.0, 1.0)
+CYAN = (0.0, 1.0, 1.0)
+DARK_CYAN = (0.0, 0.75, 0.75)
+LIGHT_MAGENTA = (1.0, 0.75, 1.0)
+MAGENTA = (1.0, 0.0, 1.0)
+DARK_MAGENTA = (0.75, 0.0, 0.75)
+
+
