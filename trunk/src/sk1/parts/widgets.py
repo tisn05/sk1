903699ba55a962aa9fa7424a6456376a2b596d7d
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@5490c38a-2b8a-11de-a28d-194e1977add9>
2014-10-01 18:06:44 +0000

    KeepRationLabel refactored into parts package

---
+++ trunk/src/sk1/parts/widgets.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2014 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import wal
+
+from sk1 import _
+
+
+class KeepRatioLabel(wal.ActiveImage):
+
+	value = True
+
+	def __init__(self, master):
+		wal.ActiveImage.__init__(self, master, wal.IMG_KEEP_RATIO,
+						tooltip=_('Keep aspect ratio'), cmd=self.process_click)
+
+	def process_click(self, *args):
+		self.value = not self.value
+		if self.value:
+			self.set_image(wal.IMG_KEEP_RATIO)
+			self.set_tooltip(_('Don\'t keep aspect ratio'))
+		else:
+			self.set_image(wal.IMG_DONT_KEEP_RATIO)
+			self.set_tooltip(_('Keep aspect ratio'))
