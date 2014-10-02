153b3dbdbfe68ce77fcb97c9137188a713c1478e
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-10-02 16:44:54 +0000

    stock icon overlays

--- src/wal/image_ids.py
+++ src/wal/image_ids.py
@@ -15,12 +15,12 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import gtk
+import gtk, rc
 
 STOCK_NEW = gtk.STOCK_NEW
 STOCK_OPEN = gtk.STOCK_OPEN
 STOCK_SAVE = gtk.STOCK_SAVE
-STOCK_DONT_SAVE = 'gtk-action-dont-save'
+STOCK_DONT_SAVE = rc.STOCK_DONT_SAVE
 STOCK_SAVE_AS = gtk.STOCK_SAVE_AS
 STOCK_CLOSE = gtk.STOCK_CLOSE
 STOCK_PRINT = gtk.STOCK_PRINT
@@ -35,7 +35,10 @@ STOCK_DELETE = gtk.STOCK_DELETE
 STOCK_SELECT_ALL = gtk.STOCK_SELECT_ALL
 STOCK_ZOOM_IN = gtk.STOCK_ZOOM_IN
 STOCK_ZOOM_OUT = gtk.STOCK_ZOOM_OUT
+
+STOCK_ZOOM_PAGE = rc.STOCK_ZOOM_PAGE
 STOCK_FILE = gtk.STOCK_FILE
+
 STOCK_ZOOM_100 = gtk.STOCK_ZOOM_100
 STOCK_ZOOM_FIT = gtk.STOCK_ZOOM_FIT
 STOCK_REFRESH = gtk.STOCK_REFRESH
