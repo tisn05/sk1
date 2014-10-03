1d2c40d76a5605ab5ab0e326267f3cea5e8126a7
Igor.E.Novikov <Igor.E.Novikov@gmail.com>
2014-10-03 16:48:23 +0000

    palette refactored to use WAL

--- src/sk1/parts/palette_widget.py
+++ src/sk1/parts/palette_widget.py
@@ -15,140 +15,95 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import wal
 
-import gtk
-import cairo
-
-from sk1 import events, config
+from uc2 import uc2const
+from sk1 import events, config, rc
 
 SHIFT = 15
 
-class HPaletteWidget(gtk.DrawingArea):
+class HPaletteWidget(wal.CairoCanvas):
 
-	def __init__(self, app):
+	last_color = None
 
-		gtk.DrawingArea.__init__(self)
+	def __init__(self, master, app):
 		self.app = app
-		self.pal = self.app.palette_mngr.palette_in_use.colors
-
-		self.set_size_request(-1, config.hpalette_cell_vertical)
-		self.queue_draw()
+		size = (-1, config.hpalette_cell_vertical)
+		wal.CairoCanvas.__init__(self, master, size, has_tooltip=True)
 		self.position = 0
 		self.max_pos = 0
 		self.max_position()
-		self.add_events(gtk.gdk.BUTTON_PRESS_MASK |
-					gtk.gdk.BUTTON_RELEASE_MASK |
-              		gtk.gdk.SCROLL_MASK)
-		self.connect('expose_event', self.repaint)
-		self.connect('button-press-event', self.button_press)
-		self.connect('button-release-event', self.button_release)
-		self.connect('scroll-event', self.scroll_event)
-
-		self.set_property('has-tooltip', True)
-		self.connect('query-tooltip', self.update_tooltip)
-		self.tooltip = None
-		self.cmyk_icon = self.load_icon('cmyk_color.png')
-		self.rgb_icon = self.load_icon('rgb_color.png')
-		events.connect(events.CMS_CHANGED, self.cms_changed)
-
-	def cms_changed(self, *args):
-		self.queue_draw()
-
-	def load_icon(self, file_name):
-		image_dir = os.path.join(config.resource_dir, 'icons', 'palette')
-		loader = gtk.gdk.pixbuf_new_from_file
-		return loader(os.path.join(image_dir, file_name))
+		events.connect(events.CMS_CHANGED, self.repaint_request)
 
 	def max_position(self):
-		w, h = tuple(self.allocation)[2:]
+		pal = self.app.palette_mngr.palette_in_use.colors
+		w, h = self.get_size()
 		if w and h:
 			size = float(config.hpalette_cell_horizontal)
-			self.max_pos = len(self.pal) * size / w - 1.0
+			self.max_pos = len(pal) * size / w - 1.0
 			self.max_pos *= w / size
 
-	def scroll_event(self, *args):
-		event = args[1]
-		if event.direction == gtk.gdk.SCROLL_UP:
-			self.position += SHIFT
-			self.queue_draw()
-		if event.direction == gtk.gdk.SCROLL_DOWN:
-			self.position -= SHIFT
-		if self.position > 0: self.position = 0
-		if self.position < -self.max_pos: self.position = -self.max_pos
-		self.queue_draw()
-
-	def update_tooltip(self, *args):
-		x = args[1]
-		tooltip = args[4]
+	def update_tooltip(self, x, y):
+		pal = self.app.palette_mngr.palette_in_use.colors
 		offset = config.hpalette_cell_horizontal
 		cell = int(float(x) / float(offset) - self.position)
-		if cell > len(self.pal): return False
-		color = self.pal[cell]
-		if not color == self.tooltip:
-			self.tooltip = color
-			return False
-		markup = ''
-		if color[3]:
-			markup += '<b> ' + color[3] + '</b>\n'
-		if color[0] == 'CMYK':
-			val = color[1]
-			tooltip.set_icon(self.cmyk_icon)
-			markup += ' C-%i%% M-%i%% Y-%i%% K-%i%%' % (val[0] * 100,
-													val[1] * 100,
-													val[2] * 100,
-													val[3] * 100)
-		if color[0] == 'RGB':
-			val = color[1]
-			tooltip.set_icon(self.rgb_icon)
-			markup += ' R-%i G-%i B-%i' % (val[0] * 255,
-											val[1] * 255,
-											val[2] * 255)
-		if markup:
-			tooltip.set_markup(markup)
-			return True
-		else:
-			return False
+		if cell > len(pal): return ()
+		color = pal[cell]
+		if not color == self.last_color:
+			self.last_color = color
+			return ()
+		icon = fist = second = ''
 
+		if color[3]: fist = color[3]
+		val = color[1]
+		if color[0] == uc2const.COLOR_CMYK:
+			icon = rc.IMG_PALETTE_CMYK_COLOR
+			second = 'C-%i%% M-%i%% Y-%i%% K-%i%%' % (val[0] * 100,
+							val[1] * 100, val[2] * 100, val[3] * 100)
 
-	def button_press(self, *args):
-		event = args[1]
-		offset = config.hpalette_cell_horizontal
-		cell = int(float(event.x) / float(offset) - self.position)
-		if event.button == 1:
-			self.app.proxy.fill_selected(self.pal[cell])
-		if event.button == 3:
-			self.app.proxy.stroke_selected(self.pal[cell])
+		if color[0] == uc2const.COLOR_RGB:
+			icon = rc.IMG_PALETTE_RGB_COLOR
+			second = 'R-%i G-%i B-%i' % (val[0] * 255, val[1] * 255, val[2] * 255)
+		return (icon, fist, second)
 
-	def button_release(self, *args):
-		pass
-
-	def repaint(self, *args):
-		self.max_position()
+	def mouse_wheel(self, event):
+		self.position += SHIFT * event.direction
+		if self.position > 0: self.position = 0
 		if self.position < -self.max_pos: self.position = -self.max_pos
+		self.repaint_request()
 
-		ctx = self.window.cairo_create()
-		ctx.set_antialias(cairo.ANTIALIAS_NONE)
+	def mouse_left_down(self, event):
+		pal = self.app.palette_mngr.palette_in_use.colors
+		offset = config.hpalette_cell_horizontal
+		cell = int(float(event.pos[0]) / float(offset) - self.position)
+		self.app.proxy.fill_selected(pal[cell])
+
+	def mouse_right_down(self, event):
+		pal = self.app.palette_mngr.palette_in_use.colors
+		offset = config.hpalette_cell_horizontal
+		cell = int(float(event.pos[0]) / float(offset) - self.position)
+		self.app.proxy.stroke_selected(pal[cell])
+
+	def repaint(self):
+		self.max_position()
+		if self.position < -self.max_pos:
+			self.position = -self.max_pos
+		w = self.get_size()[0]
 
 		x0 = 0.0; y0 = 2.0
 		offset = config.hpalette_cell_horizontal
+		pal = self.app.palette_mngr.palette_in_use.colors
 		y1 = config.hpalette_cell_vertical + 1
+		self.set_antialias(False)
 
 		i = self.position
-
-		for color in self.pal:
+		for color in pal:
 			x0 = i * config.hpalette_cell_horizontal
-			ctx.rectangle(x0, y0, offset, y1)
-			r, g, b = self.app.default_cms.get_display_color(color)
-			ctx.set_source_rgb(r, g, b)
-			ctx.fill()
-			ctx.rectangle(x0, y0, offset, y1)
-			ctx.set_line_width(1.0)
-			ctx.set_source_rgb(0, 0, 0)
-			ctx.stroke()
+			if x0 > w:break
+			rect = (x0, y0, offset, y1)
+			color = self.app.default_cms.get_display_color(color)
+			self.rectangle(rect, color, wal.BLACK)
 			i += 1
-
-
 
 
 
